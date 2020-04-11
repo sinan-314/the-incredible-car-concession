20c2ac807fa474a14b7c0b5c62ec1f20f165ddbb
sinan-314 <58488595+sinan-314@users.noreply.github.com>
2020-04-11 15:39:28 +0200

    main.cpp structure

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+#include "headers/car.h"
+#include "headers/moto.h"
+#include "headers/vehicule.h"
+
+using namespace std;
+
+int main()
+{
+
+  return (0);
+}
